ce566e8c662d235adc971bef9c3edb1fbd630d63
aleem1690-hm-01 <152846166+aleem1690-hm-01@users.noreply.github.com>
2023-12-20 03:00:32 +0530

    Update ProdRecom.py

--- ProdRecom.py
+++ ProdRecom.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 class ProdRecom:
   def __init__(self):
-    openai.api_key = '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'
+    openai.api_key = st.secrets["API_KEY"]
     self._no_of_links = 2
 
   def req_summary(self,search_request):
